06c11b155a481dfb24e5520326971ee29b382228
cipoale <103267360+cipoale@users.noreply.github.com>
2022-04-11 10:28:10 +0200

    Update PersonDao.java

--- Rubrica/src/main/java/com/aesys/rubrica/dao/PersonDao.java
+++ Rubrica/src/main/java/com/aesys/rubrica/dao/PersonDao.java
@@ -22,52 +22,26 @@ public class PersonDao {
     
     public List<Persona> getListaPersone() throws ClassNotFoundException, SQLException{
         
-        List<Persona> listaPersone = new ArrayList<>();
         
-        
-        Connection conn = ConnessioneDao.getConnection();
-        String query = "select * from persona";
-        PreparedStatement ps = conn.prepareStatement(query);
-            
-        ResultSet rs = ps.executeQuery();
-            
-        while (rs.next()){
-            
-            listaPersone.add(new Persona(rs.getString("nome"),
-                                        rs.getString("cognome"),
-                                        rs.getString("telefono"))) ;
-        }
-        
-        return listaPersone;
+        return null;
     }
     
     public static boolean insert(Persona persona) throws ClassNotFoundException, SQLException{
         
-        Connection conn = ConnessioneDao.getConnection();
-        Statement statement = conn.createStatement();
-        String query = "INSERT INTO persona VALUES ( '" + 
-                persona.getNome() + "','" + 
-                persona.getCognome() + "','" +
-                persona.getTelefono() + "')";
-        statement.executeUpdate(query);
+        
 
         return true;
     }
     
     public static boolean delete(Persona persona) throws ClassNotFoundException, SQLException{
         
-        Connection conn = ConnessioneDao.getConnection();
-        Statement statement = conn.createStatement();
-        String query = "DELETE FROM persona WHERE ( nome='" + 
-                persona.getNome() + "'AND cognome='" + 
-                persona.getCognome() + "'AND telefono='" +
-                persona.getTelefono() + "')";
-        statement.executeUpdate(query);
+        
 
         return true;
     }
     
     public static boolean update(Persona persona) throws ClassNotFoundException, SQLException{
+        
         
         return true;
         
